fffd89b4c279576f3e43cd0e2f2f055b2f62fcb9
Muhammad Haikal Ramli <2024783583@isiswa.uitm.edu.my>
2026-01-20 23:02:25 +0800

    Fix Heroku compatibility: migrate from Jakarta EE to javax.servlet for Tomcat 9

--- src/main/java/controller/SignUpController.java
+++ src/main/java/controller/SignUpController.java
@@ -3,11 +3,11 @@ package controller;
 import dao.ClientDAO;
 import model.Client;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet(name = "SignUpController", urlPatterns = {"/SignUpController", "/client/register"})
--- src/main/java/servlet/SettingsServlet.java
+++ src/main/java/servlet/SettingsServlet.java
@@ -3,12 +3,12 @@ package servlet;
 import dao.PhotographerDAO;
 import model.Photographer;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "SettingsServlet", urlPatterns = {"/settings", "/SettingsServlet"})
